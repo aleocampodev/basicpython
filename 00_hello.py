d5362b58c02ef188dcdc1107ead637ce025207b4
Aleja Ocampo <omaira.527@hotmail.com>
2024-06-03 14:21:43 -0500

    upload files

---
+++ 00_hello.py
@@ -0,0 +1,17 @@
+#Hola mundo
+print('Hola python')
+"""
+ Esto es un comentario multilinea
+"""
+
+'''
+ Esto es un comentario multilinea
+'''
+
+
+#Consultar el tipo de dato
+print(type("Soy un dato str")) #Tipo str
+print(type(5))# Tipo int
+print (type(1.5)) #Tipo float
+print (type(1 + 5j)) #Tipo float
+print(type(True)) #Tipo boolean
---
+++ 01_variables.py
@@ -0,0 +1,20 @@
+#My variables
+#Python no compila interpreta
+MyVariable = "My String Variable"
+print(MyVariable)
+
+#snake case, manera correcta
+my_variable = "string"
+
+my_int_variable= 6
+my_bool_variable = false
+print(type(my_int_variable))
+
+#Print puede llevar diferentes argumentos, Concatenacion de variables
+print(MyVariable, my_variable)
+
+print(my_variable, str(my_int_variable))
+print("Este es el valor de:", my_)
+
+#Funciones del sistema
+print(len(my_variable))
