23e0498e64c27aee6f95803ef18d9f2913466f4c
Aleja Ocampo <omaira.527@hotmail.com>
2024-06-09 16:06:14 -0500

    add more topics

--- 07_dicts.py
+++ 07_dicts.py
@@ -38,3 +38,48 @@ my_list = ["Nombre", 1, "Piso"]
 my_new_dict = dict.fromkeys(my_list)
 #my_new_dict = dict.fromkeys((my_list))
 print(my_new_dict)
+my_dict["calle"] = "Alejandra Calle"
+print(my_dict)
+
+###del my_dict["Calle"]
+###print(my_dict)
+
+
+print("Aleja" in my_dict) #false
+print("Nombre" in my_dict) #true
+
+
+print(my_dict.items()) 
+print(my_dict.keys())
+print(my_dict.values()) 
+print(my_dict.fromkeys("Nombre", 1)) #se tiene un par de keys
+
+#crear un dicts sin valores
+my_new_dict = my_other_dict.fromkeys(("Nombre", 1))
+print(my_new_dict)
+
+my_new_dict = dict.fromkeys(("Nombre", 1, "Piso"))
+
+my_list = ["Nombre", 1, "Piso"]
+my_new_dict = dict.fromkeys((my_list))
+print(my_new_dict)
+
+my_new_dict = dict.fromkeys(("Nombre", 1, "Piso"))
+print(my_new_dict)
+
+my_new_dict = dict.fromkeys(my_dict)
+print(my_new_dict)
+
+my_new_dict = dict.fromkeys(my_dict, ("Ale", "Ocampo"))
+#my_new_dict = dict.fromkeys(my_dict, ["Ale", "Ocampo"]))
+print(my_new_dict)
+
+my_new_dict = dict.fromkeys(my_dict,  "AleDev")
+print(my_new_dict)
+
+print(list(my_new_dict.values()))
+print(tuple(my_new_dict))
+print(set(my_new_dict))
+
+print(list(dict.fromkeys(list(my_new_dict.values()))))
+print(list(dict.fromkeys(list(my_new_dict.keys()))))
